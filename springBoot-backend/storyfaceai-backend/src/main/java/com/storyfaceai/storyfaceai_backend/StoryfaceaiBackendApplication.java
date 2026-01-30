cfff9ab71b89291428dc9cc5aa9d315387c70353
Ayush <prakashayush414@gmail.com>
2026-01-30 21:19:18 +0530

    Adding Spring backend

---
+++ springBoot-backend/storyfaceai-backend/src/main/java/com/storyfaceai/storyfaceai_backend/StoryfaceaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.storyfaceai.storyfaceai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryfaceaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryfaceaiBackendApplication.class, args);
+	}
+
+}
---
+++ springBoot-backend/storyfaceai-backend/src/test/java/com/storyfaceai/storyfaceai_backend/StoryfaceaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storyfaceai.storyfaceai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryfaceaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
